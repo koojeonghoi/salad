e80aaab6ca3b398e50042022dadc874168f396c7
koojeonghoi <83147741+koojeonghoi@users.noreply.github.com>
2026-07-22 14:36:11 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCEClhQx10lSiXEbmT7XfD9QspUoA4WGbc",
-  authDomain: "roulette-d9a12.firebaseapp.com",
-  databaseURL: "https://roulette-d9a12-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "roulette-d9a12",
-  storageBucket: "roulette-d9a12.firebasestorage.app",
-  messagingSenderId: "1068316590225",
-  appId: "1:1068316590225:web:fcabda3bc7ea8192267f30"
-};
+export const firebaseConfig = {2  apiKey: "AIzaSyCEClhQx10lSiXEbmT7XfD9QspUoA4WGbc",3  authDomain: "roulette-d9a12.firebaseapp.com",4  databaseURL: "https://roulette-d9a12-default-rtdb.asia-southeast1.firebasedatabase.app",5  projectId: "roulette-d9a12",6  storageBucket: "roulette-d9a12.firebasestorage.app",7  messagingSenderId: "1068316590225",8  appId: "1:1068316590225:web:fcabda3bc7ea8192267f30"9};
