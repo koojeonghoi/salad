0b14660e2f92280a9e4b9cb9624db987e7883dee
koojeonghoi <83147741+koojeonghoi@users.noreply.github.com>
2026-07-22 13:09:38 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEClhQx10lSiXEbmT7XfD9QspUoA4WGbc",
+  authDomain: "roulette-d9a12.firebaseapp.com",
+  databaseURL: "https://roulette-d9a12-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "roulette-d9a12",
+  storageBucket: "roulette-d9a12.firebasestorage.app",
+  messagingSenderId: "1068316590225",
+  appId: "1:1068316590225:web:fcabda3bc7ea8192267f30"
+};
